67c8bbfb0c95115fe5c5cbe6234bf8bb2720807f
LevonHar <harutyunyanlevon99@gmail.com>
2025-07-10 19:07:24 +0400

    utils-constants

---
+++ lib/utils/device/device_utility.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class TDeviceUtils {
+  static void hideKeyboard(BuildContext context) {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  static Future<void> setStatusBarColor(Color color) async {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: color),
+    );
+  }
+
+  static bool isLandscapeOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom == 0;
+  }
+
+  static bool isPortraitOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom != 0;
+  }
+
+  static void setFullScreen(bool enable) {
+    SystemChrome.setEnabledSystemUIMode(enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge);
+  }
+
+  static double getScreenHeight() {
+    return MediaQuery.of(Get.context!).size.height;
+  }
+}
